fb2b94baecd9fe558906e8ec6f7b708858d0281b
Fabia Omezzoli <>
2018-09-25 09:40:20 +0200

    improve notification

--- notificationGenerator.php
+++ notificationGenerator.php
@@ -49,6 +49,7 @@ function generateNotification(){
 
 add_action( 'init', 'notificationFormSubmit');
 
+
 function notificationFormSubmit() {
 
     if( isset( $_POST['submitbtn'] ) ) {
@@ -57,7 +58,7 @@ function notificationFormSubmit() {
         $scheduledDate =$_POST['scheduledDate'];
         $current_user = wp_get_current_user();
 
-        $my_post = array(
+        $newWPNotification = array(
             'post_type'     => 'notification',
             'post_title'    => $_POST['notificationTitle'],
             'post_content'  => $_POST['notificationContent'],
@@ -65,10 +66,83 @@ function notificationFormSubmit() {
             'post_author'   => $current_user->ID,
         );
         // Insert the notification into the database.
-        $postId=wp_insert_post( $my_post );
-        if($postId!=0){
-            update_post_meta($postId,'usersList'    ,$usersList);
-            update_post_meta($postId,'scheduledDate',$scheduledDate);
+        $newWPNotificationId=wp_insert_post( $newWPNotification );
+        if($newWPNotificationId!=0){
+            add_post_meta($newWPNotificationId,'usersList'    ,$usersList);
+            add_post_meta($newWPNotificationId,'scheduledDate',$scheduledDate);
+            createOneSignalNotification($newWPNotificationId);
         }
     }
+}
+
+function createOneSignalNotification($notificationWPId){
+
+    $players       = array();
+    $usersList     = array();
+    $scheduledDate = array();
+    $usersList     = get_post_meta($notificationWPId,'usersList',true);
+    $scheduledDate = get_post_meta($notificationWPId,'scheduledDate',true);
+
+
+    foreach($usersList as $user) {
+        $array = get_user_meta($user, 'onesignal_id');
+        if (!empty($array)) {
+            foreach ($array as $v) {
+                array_push($players, $v);
+            }
+        }
+    }
+
+
+    $onesignal_wp_settings = OneSignal::get_onesignal_settings();
+    $header =array(
+        'authorization: Basic '.$onesignal_wp_settings['app_rest_api_key'] ,
+        'content-type: application/json',
+    );
+    $curl = curl_init();
+
+    $notificationContent    = get_post($notificationWPId);
+    $OSnotificationContent  = $notificationContent->post_content;
+
+
+    // CREATE BODY
+    $body = array(
+        'app_id' => $onesignal_wp_settings['app_id'] ,
+        'headings' => array(
+            'en' => get_the_title($notificationWPId),
+            'it' => get_the_title($notificationWPId),
+        ),
+        'contents' => array(
+            'en' => $OSnotificationContent,
+            'it' => $OSnotificationContent,
+        ),
+        'include_player_ids' => $players,
+        'send_after' => $scheduledDate,
+    );
+
+
+    // SEND REQUEST TO ONESIGNAL
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://onesignal.com/api/v1/notifications",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($body),
+        CURLOPT_HTTPHEADER => $header,
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    // CLOSE CONNECTION
+    curl_close($curl);
+
+    // CHECK FOR ERRORS
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        echo $response.$scheduledDate;
+    };
 }
